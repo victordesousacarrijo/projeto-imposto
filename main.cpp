5ecdcf38a0ff4500e0e688e9478a59b72a4e7001
Victor de Sousa Carrijo <victorcarrijo96@gmail.com>
2020-05-16 15:14:42 -0300

    Leitura dos dados

--- main.cpp
+++ main.cpp
@@ -3,4 +3,17 @@ using namespace std;
 
 
 int main() {
+    double RendaSalario, RendaServical, RendaCapital, GastoMédico, GastoEducacional;
+    cout << "Renda Anual com Salário:" ;
+    cin >> RendaSalario;
+     cout << "Renda anual com prestação de serviço: ";
+     cin >> RendaServical;
+     cout << "Renda anual com ganho de capital: ";
+     cin >> RendaCapital;
+     cout << "Gastos médicos: ";
+     cin >> GastoMédico;
+     cout << "Gastos educacionais: ";
+     cin >> GastoEducacional;
+
+
 }
